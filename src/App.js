90d5ce481510d7c0985681b15bc7fcbe500eb57b
unknown <nicolasgonzalez470@gmail.com>
2022-08-24 13:30:37 -0300

    app style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import styled from 'styled-components'
 
 function App() {
 
+  const AppStyled = styled.main`
+    background-image: radial-gradient(circle at top, #1f3757 20%, #131537 100%);
+
+    .app-content{
+      min-height: 100vh;
+    }
+  `
+
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled>
+      <div className='app-content'>
+
+      </div>
+    </AppStyled>
   );
 }
 
